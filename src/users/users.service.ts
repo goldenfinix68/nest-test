445799f6d248a3e2439126c5c7415d78cd49a298
Golden Phoenix <goldenfinix@outlook.com>
2024-08-20 04:28:10 -0400

    Add userService in user Module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { RabbitmqService } from '../rabbitmq/rabbitmq.service';
+import { EmailService } from '../email/email.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private readonly rabbitMQService: RabbitmqService,
+    private readonly emailService: EmailService,
+  ) {}
+
+  async createUser(createUserDto: any): Promise<User> {
+    const savedUser = await this.userModel.create(createUserDto);
+
+    // Send a dummy email
+    this.emailService.sendEmail(savedUser.email);
+
+    // Publish a dummy event to RabbitMQ
+    this.rabbitMQService.publishEvent(savedUser);
+
+    return savedUser;
+  }
+}
